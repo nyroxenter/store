81f957e0fc8c64ebec20160159503981379c79c6
NyroxEnter <mastercraft575@gmail.com>
2026-08-01 13:47:16 +0400

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,56 @@
+function toggleMenu(){
 
+
+    let menu =
+    document.querySelector(".side-menu");
+
+
+
+    if(menu.style.display==="block"){
+
+        menu.style.display="none";
+
+    }
+
+    else{
+
+        menu.style.display="block";
+
+    }
+
+
+}
+
+
+
+function openCategory(page){
+
+
+    let user =
+    localStorage.getItem("nyroxhubUser");
+
+
+
+    if(!user){
+
+
+        alert(
+        "Խնդրում ենք նախ գրանցվել կամ մուտք գործել"
+        );
+
+
+        window.location.href="../register.html";
+
+
+        return;
+
+
+    }
+
+
+
+    window.location.href =
+    "categories/"+page;
+
+
+}
