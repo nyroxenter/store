8ce9e3b6225ac77d75a8711c24ac355b40db1a92
NyroxEnter <mastercraft575@gmail.com>
2026-08-01 13:46:42 +0400

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,98 @@
+function registerUser(){
 
+    let username =
+    document.querySelector("#username").value;
+
+    let email =
+    document.querySelector("#email").value;
+
+    let password =
+    document.querySelector("#password").value;
+
+
+    if(!username || !email || !password){
+
+        alert("Խնդրում ենք լրացնել բոլոր դաշտերը");
+        return;
+
+    }
+
+
+    let user = {
+
+        username: username,
+        email: email,
+        password: password,
+        avatar: "assets/avatar.png"
+
+    };
+
+
+    localStorage.setItem(
+        "nyroxhubUser",
+        JSON.stringify(user)
+    );
+
+
+    alert("Գրանցումը հաջողվեց");
+
+
+    window.location.href="profile.html";
+
+}
+
+
+
+function loginUser(){
+
+
+    let email =
+    document.querySelector("#email").value;
+
+
+    let password =
+    document.querySelector("#password").value;
+
+
+
+    let savedUser =
+    JSON.parse(localStorage.getItem("nyroxhubUser"));
+
+
+
+    if(savedUser && 
+       savedUser.email === email &&
+       savedUser.password === password){
+
+
+        alert("Բարի գալուստ NyroxHub");
+
+
+        window.location.href="profile.html";
+
+
+    }
+
+    else{
+
+
+        alert("Սխալ տվյալներ");
+
+
+    }
+
+
+}
+
+
+
+function logout(){
+
+
+    localStorage.removeItem("nyroxhubUser");
+
+
+    window.location.href="index.html";
+
+
+}
