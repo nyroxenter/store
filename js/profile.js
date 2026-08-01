2c7aa1ddfaee79f12ca136183ae0dc76d91ed4ef
NyroxEnter <mastercraft575@gmail.com>
2026-08-01 13:48:13 +0400

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,1 +1,109 @@
+function loadProfile(){
 
+
+let user =
+
+JSON.parse(
+
+localStorage.getItem("nyroxhubUser")
+
+);
+
+
+
+if(!user){
+
+
+window.location.href="login.html";
+
+return;
+
+
+}
+
+
+
+let username =
+document.querySelector("#profileName");
+
+
+let avatar =
+document.querySelector("#profileAvatar");
+
+
+
+if(username){
+
+username.innerHTML =
+user.username;
+
+}
+
+
+
+if(avatar){
+
+avatar.src =
+user.avatar;
+
+}
+
+
+
+}
+
+
+
+function changeAvatar(){
+
+
+let url =
+
+prompt(
+"Տեղադրեք նկարի հղումը"
+);
+
+
+
+if(!url) return;
+
+
+
+let user =
+
+JSON.parse(
+
+localStorage.getItem("nyroxhubUser")
+
+);
+
+
+
+user.avatar=url;
+
+
+
+localStorage.setItem(
+
+"nyroxhubUser",
+
+JSON.stringify(user)
+
+);
+
+
+
+location.reload();
+
+
+}
+
+
+
+window.onload=function(){
+
+loadProfile();
+
+loadNews();
+
+}
