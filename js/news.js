adbd5c858d0559165fb6662aa34bb2180b275e36
NyroxEnter <mastercraft575@gmail.com>
2026-08-01 13:47:45 +0400

    Update news.js

--- js/news.js
+++ js/news.js
@@ -1,1 +1,125 @@
+function createNews(){
 
+
+let title =
+document.querySelector("#newsTitle").value;
+
+
+let description =
+document.querySelector("#newsDescription").value;
+
+
+
+if(!title || !description){
+
+
+alert("Լրացրեք բոլոր դաշտերը");
+
+return;
+
+
+}
+
+
+
+let news = {
+
+
+title:title,
+
+description:description,
+
+date:new Date().toLocaleDateString()
+
+
+};
+
+
+
+let oldNews =
+JSON.parse(
+localStorage.getItem("nyroxhubNews")
+) || [];
+
+
+
+oldNews.push(news);
+
+
+
+localStorage.setItem(
+
+"nyroxhubNews",
+
+JSON.stringify(oldNews)
+
+);
+
+
+
+alert(
+"Նորությունը ստեղծվեց"
+);
+
+
+
+location.reload();
+
+
+}
+
+
+
+function loadNews(){
+
+
+let box =
+document.querySelector(".news-container");
+
+
+
+if(!box) return;
+
+
+
+let news =
+
+JSON.parse(
+
+localStorage.getItem("nyroxhubNews")
+
+) || [];
+
+
+
+news.forEach(item=>{
+
+
+box.innerHTML += `
+
+<div class="card">
+
+<h2>
+${item.title}
+</h2>
+
+
+<p>
+${item.description}
+</p>
+
+
+<small>
+${item.date}
+</small>
+
+
+</div>
+
+`;
+
+
+});
+
+
+}
